f7c21a50897a1f93284f14a113331cda016f1d11
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2023-12-23 16:18:24 +0100

    added prompt answer to prompts, implemented author parsing

--- content_scripts/content_script.js
+++ content_scripts/content_script.js
@@ -1,28 +1,46 @@
-  browser.runtime.onMessage.addListener((request, sender) => {
+browser.runtime.onMessage.addListener((request, sender) => {
+    console.log("Received a message from the popup: ", request);
+
     if (request.action === "fetchPrompts") {
-        let prompts = getUserPrompts();
-        return Promise.resolve({ prompts: prompts });
+        return Promise.resolve({ prompts: getPrompts() });
     } else if (request.action === "generateCitation") {
         let citation = generateBibTeX(request.entry);
         return Promise.resolve({ citation: citation });
     }
     return false;
-  });
-  
+});
 
-function getUserPrompts() {
-    let allUserPrompts = [];
-    
-    // Query the document for elements that contain user messages
-    let userMessageElements = document.querySelectorAll('div[data-message-author-role="user"]');
 
-    userMessageElements.forEach(element => {
-        // Extract the text content of each message element
-        let messageText = element.textContent || element.innerText;
-        allUserPrompts.push(messageText.trim());
-    });
+function getPrompts() {
+    let presentation = document.querySelector('div[role="presentation"]');
 
-    return allUserPrompts;
+    if (!presentation) {
+        return [];
+    }
+
+    let chat = presentation.children[0];
+
+
+    if (!chat) {
+        return [];
+    }
+
+    // get all children with theses classes w-full text-token-text-primary
+    let messages = chat.querySelectorAll('div[class="w-full text-token-text-primary"]');
+
+
+    let prompts = [];
+
+    for (let i = 0; i < messages.length; i++) {
+        //if i is uneven whe have a new prompt answer pair
+        if ((i + 1) % 2 == 1) {
+            prompts.push({ prompt: messages[i].querySelector('div[class=""]').textContent.trim(), answer: "" });
+        } else {
+            prompts[prompts.length - 1].answer = messages[i].querySelector('p').textContent.trim();
+        }
+    }
+
+    return prompts;
 }
 
 
@@ -36,7 +54,7 @@ function generateBibTeX(entries) {
         if (typeof entry !== "object") {
             return "";
         }
-    
+
         let citation = "";
 
         // Start the entry
@@ -74,5 +92,46 @@ function generateBibTeX(entries) {
     });
 
     return allEntriesBibTeX;
-    
+
+}
+
+/**
+ * Returns the GPT version used in the chat
+ * 
+ * @returns {string} the GPT version used in the chat
+ */
+function getGPTver() {
+    let titleBar = document.querySelector('div[class="sticky top-0 mb-1.5 flex items-center justify-between z-10 h-14 bg-white p-2 font-semibold dark:bg-gray-800"]');
+
+    let gptVer = titleBar.children[1].children[0].children[0].innerText;
+
+    if (!gptVer) {
+        let gptName = document.querySelector('div[class="group flex cursor-pointer items-center gap-1 rounded-xl py-2 px-3 text-lg font-medium hover:bg-gray-50 radix-state-open:bg-gray-50 dark:hover:bg-black/10 dark:radix-state-open:bg-black/20"]');
+
+        return gptName.innerText;
+    }
+
+    return gptVer
+}
+
+function getAuthors() {
+    const gptVer = getGPTver();
+
+    if (!gptVer) {
+        return undefined;
+    }
+
+    if (gptVer == "ChatGPT Plugins") {
+        return "OpenAI gpt-4-plugins"
+    }
+
+    if (gptVer == "ChatGPT 4") {
+        return "OpenAI gpt-4"
+    }
+
+    if (gptVer == "ChatGPT 3.5") {
+        return "OpenAI gpt-3.5"
+    }
+
+    return "OpenAI CustomGPT gpt-4 " + gptVer.replaceAll(" ", "-").toLowerCase();
 }
